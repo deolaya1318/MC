6de953177376f5e11ead35934361f2d06ac99c4e
deolaya1318 <de.olaya1318@uniandes.edu.co>
2015-06-09 23:39:23 -0500

    add first part for first point of HW2

--- Tareas/HW2/circle.sh
+++ Tareas/HW2/circle.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+#convierte el sript de bash por un script de gnuplot
 gnuplot<<EOF
+#guarda las variables que determinan el radio y los limites de la grafica para que el circulo tenga su centro en el origen del marco coordenado
 r=$1
 xlim=1
 ylim=1
+#configuracion para graficar en la terminal, para parametrizar la funcion a graficar, para observar la grafica a escala 1 a 1, y para designar los limites de ambos ejes
 set term dumb
 set parametric
 set size ratio 0.5
 set xrange [xlim:-xlim]
 set yrange [ylim:-ylim]
+#grafica el circulo con radio r dado por la terminal
 plot r*cos(t),r*sin(t) 
 EOF
+#volvemos a un script en bash
