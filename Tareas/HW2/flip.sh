340e7c8e7c771fe83c059da1c76595a36861186d
deolaya1318 <de.olaya1318@uniandes.edu.co>
2015-06-09 23:42:33 -0500

    add first part for third point of HW2

--- Tareas/HW2/flip.sh
+++ Tareas/HW2/flip.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#El script debe recibir tres parametros: el nombre del archivo y las dos letras. Su salida se ve en la terminal.
+#El script debe recibir tres parametros: el nombre del archivo y las dos letras. Su salida se ve en la terminal
 cat $1 | tr "$2$3" "$3$2"
+#fin del script
